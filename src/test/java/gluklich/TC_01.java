40a8683860afda3f20cf75b1f3abf3df2cabd7f9
Anusham3 <anuanusham3@gmail.com>
2020-03-02 17:21:01 +0530

    2nd commit

--- src/test/java/gluklich/TC_01.java
+++ src/test/java/gluklich/TC_01.java
@@ -1,5 +1,7 @@
 package gluklich;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -10,17 +12,18 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TC_01 {
 	
-	/*static
+	static
 	{
-		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
-	}*/
+		System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
+	}
 	
 	@Test
 	
 	 public void testOne()
 	 {
-		WebDriverManager.chromedriver().version("76").setup();
+		//WebDriverManager.chromedriver().version("76").setup();
 		WebDriver driver=new ChromeDriver();
+		//driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
 		driver.get("https://www.google.com");
 		System.out.println(driver.getTitle());
 		driver.close();
